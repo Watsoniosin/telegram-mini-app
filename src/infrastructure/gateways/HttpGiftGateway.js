11962c2beae69855ac7959b54bf35508b6995ddd
Watsoniosin <watsonvitaliy@gmail.com>
2025-11-04 16:13:27 +0200

    fix: CORS proxy for api.changes.tg

--- src/infrastructure/gateways/HttpGiftGateway.js
+++ src/infrastructure/gateways/HttpGiftGateway.js
@@ -1,17 +1,19 @@
 // src/infrastructure/gateways/HttpGiftGateway.js
 export class HttpGiftGateway {
   constructor() {
+    // ПРОКСІ, ЯКИЙ ДОДАЄ CORS
+    this.proxy = 'https://corsproxy.io/?';
     this.baseUrl = 'https://api.changes.tg/original';
   }
 
   async loadGiftAnimation(giftId) {
-    const url = `${this.baseUrl}/${giftId}.json`;
+    const url = this.proxy + encodeURIComponent(`${this.baseUrl}/${giftId}.json`);
     const response = await fetch(url);
     if (!response.ok) throw new Error(`Gift not found: ${giftId}`);
     return await response.json();
   }
 
   loadGiftPreview(giftId) {
-    return `${this.baseUrl}/${giftId}.png`;
+    return this.proxy + encodeURIComponent(`${this.baseUrl}/${giftId}.png`);
   }
 }
